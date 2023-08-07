dc3fd6fd47b176861eabbb82c0b7dbfa74c8269c
raghav nawandhar <nawandharraghav@gmail.com>
2023-08-07 23:04:50 +0530

    new class in util package -> StatusResponse

---
+++ src/main/java/com/fee/util/StatusResponse.java
@@ -0,0 +1,8 @@
+package com.fee.util;
+
+public class StatusResponse {
+	public final String ROLE_SUPER_ADMIN = "SUPER ADMIN";
+	public final String ROLE_ADMIN = "ADMIN";
+	public final String ROLE_USER = "USER";
+  
+}
