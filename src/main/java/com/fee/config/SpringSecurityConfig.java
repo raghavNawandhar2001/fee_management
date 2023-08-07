db1e2b45c87799a80bcb53fd1d554b8f78439c61
raghav nawandhar <nawandharraghav@gmail.com>
2023-08-07 10:43:38 +0530

    change in role entity

--- src/main/java/com/fee/config/SpringSecurityConfig.java
+++ src/main/java/com/fee/config/SpringSecurityConfig.java
@@ -1,13 +1,14 @@
 package com.fee.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.fee.Entity.Student;
+
 public class SpringSecurityConfig {
-//	@Bean
-//	PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
 
 }
